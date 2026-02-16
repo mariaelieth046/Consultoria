779f5088cd060a9884f5deaef4a7bac39b4d6d70
mariaelieth046 <mariaelieth046@gmail.com>
2026-02-15 22:03:40 -0300

    Create QuizQuestions.js

---
+++ QuizQuestions.js
@@ -0,0 +1,42 @@
+export const QuizQuestions = [
+    { 
+        text: "Como você prefere começar o seu dia?", 
+        options: ["Com energia e movimento", "De forma calma", "Com planejamento", "Com foco total"] 
+    },
+    { 
+        text: "Qual clima mais combina com você?", 
+        options: ["Sol e praia", "Dias frescos", "Noites elegantes", "Tardes aconchegantes"] 
+    },
+    { 
+        text: "Sua fragrância deve ser notada a qual distância?", 
+        options: ["Só no abraço", "Por quem passa perto", "Ao entrar no ambiente", "Deve deixar rastro"] 
+    },
+    { 
+        text: "Qual cor define seu estilo?", 
+        options: ["Azul claro/Branco", "Verde/Tons terra", "Preto/Vermelho", "Dourado/Vibrante"] 
+    },
+    { 
+        text: "O que você mais recebe elogios?", 
+        options: ["Pela leveza", "Pela elegância", "Pela atitude", "Pelo estilo moderno"] 
+    },
+    { 
+        text: "Em qual ocasião você mais usa perfume?", 
+        options: ["Trabalho/Dia a dia", "Encontros", "Festas e eventos", "Lazer ao ar livre"] 
+    },
+    { 
+        text: "Qual dessas texturas te agrada mais?", 
+        options: ["Água fresca", "Veludo macio", "Seda", "Madeira nobre"] 
+    },
+    { 
+        text: "Como quer ser descrito?", 
+        options: ["Acessível/Solar", "Clássico/Polido", "Misterioso/Atraente", "Moderno/Ousado"] 
+    },
+    { 
+        text: "Qual sua relação com perfumes doces?", 
+        options: ["Evito", "Gosto se for leve", "Amo os envolventes", "Quanto mais doce, melhor"] 
+    },
+    { 
+        text: "Escolha uma última palavra-chave:", 
+        options: ["Frescor", "Equilíbrio", "Poder", "Sedução"] 
+    }
+];
